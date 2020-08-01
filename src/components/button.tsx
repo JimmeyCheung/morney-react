709a10e4f3f8910f93f6b69568f67d77af2e1c5c
jimmeyCheung <100178981@qq.com>
2020-08-01 11:57:40 +0800

    安装styled-component

---
+++ src/components/button.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const button = styled.button`
+    color:red;
+    border:none;
+    background-color:blue;
+    &.hover{
+        background:black;
+    }
+`;
+export default button;
