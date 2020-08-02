d94e77beff05ea46eb28d6e505c9014b858f29d2
jimmeyCheung <100178981@qq.com>
2020-08-02 21:53:01 +0800

    Icon样式修改

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ import Icon from './Icon';
 
 
 const NavWrapper = styled.nav`
-  line-height: 24px;
+   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
     display:flex;
@@ -18,9 +18,13 @@ const NavWrapper = styled.nav`
         padding: 4px 0;
         justify-content: center;
         align-items: center;
-      }
-      >.selected{
-        color:red;
+        .icon {
+          width: 24px;      
+          height: 24px;
+        }
+        &.selected{
+          color: red;
+        }
       }
     }
   }
