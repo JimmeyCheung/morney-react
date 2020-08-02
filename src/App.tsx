c3b25012cffc9af4059fdf9b83b71b8294c06536
jimmeyCheung <100178981@qq.com>
2020-08-02 15:37:47 +0800

    添加路由导航

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+          <Redirect exact from="/" to="money" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Tags() {
+  return <h2>Route</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+function NoMatch() {
+  return <div>当前页面不存在</div>
+}
