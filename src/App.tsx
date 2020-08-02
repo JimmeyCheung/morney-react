c3fb37db07eff475fadea99fd4c92500e7c67997
jimmeyCheung <100178981@qq.com>
2020-08-02 22:08:17 +0800

    完成TasSection UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,37 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import Layout from 'components/Layout';
+import styled from 'styled-components';
 import Tags from 'views/tags';
 import Statistics from 'views/Statistics';
 import NoMatch from 'components/NoMatch';
 import Money from 'views/Money';
 
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags />
-        </Route>
-        <Route path="/money">
-          <Money />
-        </Route>
-        <Route path="/statistics">
-          <Statistics />
-        </Route>
-        <Redirect exact from="/" to="/money" />
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 export default App;
