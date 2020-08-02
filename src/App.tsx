72e3015c7dcdd02d3df4a78c81412b20b9885eb6
jimmeyCheung <100178981@qq.com>
2020-08-02 21:05:11 +0800

    添加Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,29 @@ import {
   Link,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Layout from 'components/Layout';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from="/" to="/money" />
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Layout>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Layout>
     </Router>
   );
 }
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try { importAll(require.context('icons', true, /\.svg$/)); } catch (error) { console.log(error); }
 
-type Prop = {
+type Props = {
     name: string
 }
-const Icon = (prop: Prop) => {
+const Icon = (props: Props) => {
     return (
         <svg className="icon">
-            <use xlinkHref={'#' + prop.name} />
+            <use xlinkHref={'#' + props.name} />
         </svg>
     );
 }
