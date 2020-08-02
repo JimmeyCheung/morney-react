0c101cea2160dffdb745e749163ad47d3f3e552f
jimmeyCheung <100178981@qq.com>
2020-08-02 16:01:01 +0800

    完成导航布局

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,52 @@ import {
   Route,
   Link,
   Redirect
-} from "react-router-dom";
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display:flex;
+    > li{
+      width: 33.3333%;
+      text-align:center;
+      padding: 16px;
+    }
+  }
+`;
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
           <ul>
             <li>
               <Link to="/tags">标签页</Link>
@@ -23,23 +62,8 @@ export default function App() {
               <Link to="/statistics">统计页</Link>
             </li>
           </ul>
-        </nav>
-        <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-          <Redirect exact from="/" to="money" />
-        </Switch>
-      </div>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
