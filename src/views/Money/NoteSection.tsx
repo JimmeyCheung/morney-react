e7ae6ca0e446f7797c4ae5258cada51a5e777f00
jimmeyCheung <100178981@qq.com>
2020-08-03 22:19:01 +0800

    完成Tag.tsx样式

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import React, { ChangeEventHandler, useRef } from 'react';
+import React, { ChangeEventHandler } from 'react';
 import { Input } from 'components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;
   
 `;
@@ -19,7 +19,8 @@ const NoteSection: React.FC<Props> = (props) => {
   };
   return (
     <Wrapper>
-      <Input label="备注" type="text" value={note} onChange={onChange} />
+      <Input label="备注" type="text" value={note} onChange={onChange}
+        placeholder="请填写备注" />
     </Wrapper>
   );
 };
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -3,8 +3,12 @@ import { useTags } from "useTags";
 import { useParams } from "react-router-dom";
 import Layout from 'components/Layout';
 import Icon from 'components/Icon';
-import { Button } from 'components/Button';
 import styled from 'styled-components';
+import { Input } from '../components/Input';
+import { Center } from '../components/Center';
+import { Space } from '../components/Space';
+import { Button } from 'components/Button';
+
 
 type Params = {
   id: string;
@@ -17,7 +21,12 @@ const Topbar = styled.header`
   padding: 14px;
   background:white;
 `;
-const Tag: React.FC = (props) => {
+const InputWrapper = styled.div`
+  background:white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
+const Tag: React.FC = () => {
   const { findTag } = useTags();
   let { id } = useParams<Params>();
   const tag = findTag(parseInt(id));
@@ -28,15 +37,15 @@ const Tag: React.FC = (props) => {
         <span>编辑标签</span>
         <Icon />
       </Topbar>
-      <div>
-        <label>
-          <span>标签名</span>
-          <input type="text" placeholder="标签名" />
-        </label>
-      </div>
-      <div>
+      <InputWrapper>
+        <Input label="标签名" type="text" placeholder="标签名" value={tag.name} />
+      </InputWrapper>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
         <Button>删除标签</Button>
-      </div>
+      </Center>
     </Layout>
   );
 };
