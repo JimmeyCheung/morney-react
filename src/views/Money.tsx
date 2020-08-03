e125d9b60e8eab0090596f2a1be1237a7f4f55da
JimmeyCheung <100178981@qq.com>
2020-08-03 09:23:36 +0800

    完成标签的选择和增加

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,62 +1,52 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import styled from 'styled-components';
-import { TagsSection } from './Money/TagsSection';
-import { CategorySection } from './Money/CategorySection';
-import { NotesSection } from './Money/NotesSection';
-import { NumberPadSection } from './Money/NumberPadSection';
+import React from "react";
+import Layout from "../components/Layout";
+import styled from "styled-components";
+import { TagsSection } from "./Money/TagsSection";
+import { CategorySection } from "./Money/CategorySection";
+import { NotesSection } from "./Money/NotesSection";
+import { NumberPadSection } from "./Money/NumberPadSection";
 const MyLayout = styled(Layout)`
-  display:flex;
+  display: flex;
   flex-direction: column;
 `;
 const Money = () => {
-    return (
-        <div>
-            <MyLayout>
-                <TagsSection>
-                    <ol>
-                        <li>衣</li>
-                        <li>食</li>
-                        <li>住</li>
-                        <li>行</li>
-                    </ol>
-                    <button>新增标签</button>
-                </TagsSection>
-                <NotesSection>
-                    <label>
-                        <span>备注</span>
-                        <input type="text" placeholder="在这里添加备注" />
-                    </label>
-                </NotesSection>
-                <CategorySection>
-                    <ul>
-                        <li className="selected">支出</li>
-                        <li>收入</li>
-                    </ul>
-                </CategorySection>
-                <NumberPadSection>
-                    <div className="output">
-                        100
-                    </div>
-                    <div className="pad clearfix">
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>删除</button>
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
-                        <button>清空</button>
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
-                        <button className="ok">OK</button>
-                        <button className="zero">0</button>
-                        <button className="dot">.</button>
-                    </div>
-                </NumberPadSection>
-            </MyLayout>
-        </div>
-    );
+  return (
+    <div>
+      <MyLayout>
+        <TagsSection />
+        <NotesSection>
+          <label>
+            <span>备注</span>
+            <input type="text" placeholder="在这里添加备注" />
+          </label>
+        </NotesSection>
+        <CategorySection>
+          <ul>
+            <li className="selected">支出</li>
+            <li>收入</li>
+          </ul>
+        </CategorySection>
+        <NumberPadSection>
+          <div className="output">100</div>
+          <div className="pad clearfix">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>删除</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>清空</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button className="ok">OK</button>
+            <button className="zero">0</button>
+            <button className="dot">.</button>
+          </div>
+        </NumberPadSection>
+      </MyLayout>
+    </div>
+  );
 };
 export default Money;
