58404cd13ae0e2918513905d342fe0940906c35f
jimmeyCheung <100178981@qq.com>
2020-08-03 22:22:26 +0800

    添加tag的删除

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -27,16 +27,11 @@ const InputWrapper = styled.div`
   margin-top: 8px;
 `;
 const Tag: React.FC = () => {
-  const { findTag, updateTag } = useTags();
+  const { findTag, updateTag, deleteTag } = useTags();
   let { id: idString } = useParams<Params>();
   const tag = findTag(parseInt(idString));
-  return (
-    <Layout>
-      <Topbar>
-        <Icon name="left" />
-        <span>编辑标签</span>
-        <Icon />
-      </Topbar>
+  const tagContent = (tag: { id: number; name: string }) => (
+    <div>
       <InputWrapper>
         <Input label="标签名" type="text" placeholder="标签名"
           value={tag.name}
@@ -49,8 +44,22 @@ const Tag: React.FC = () => {
         <Space />
         <Space />
         <Space />
-        <Button>删除标签</Button>
+        <Button onClick={() => {
+          deleteTag(tag.id);
+        }}>删除标签</Button>
       </Center>
+    </div>
+  );
+
+  return (
+    <Layout>
+      <Topbar>
+        <Icon name="left" />
+        <span>编辑标签</span>
+        <Icon />
+      </Topbar>
+
+      {tag ? tagContent(tag) : <Center>tag 不存在</Center>}
     </Layout>
   );
 };
