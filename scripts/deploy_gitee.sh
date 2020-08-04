e728aced65780a09171521101bab9029ff692ef6
JimmeyCheung <100178981@qq.com>
2020-08-04 18:07:24 +0800

    添加deploy部署脚本

---
+++ scripts/deploy_gitee.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin https://gitee.com/JimmeyCheung/morney-react-website.git &&
+git push -u origin master -f
+cd -
