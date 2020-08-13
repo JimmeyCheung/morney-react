29658fc3d8136529745887254fce1371293e0dde
jimmeyCheung <100178981@qq.com>
2020-08-13 23:22:12 +0800

    完成RecordDetail头部

--- src/views/Statistics/RecordDetail.tsx
+++ src/views/Statistics/RecordDetail.tsx
@@ -1,18 +1,26 @@
 import styled from 'styled-components';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useRecords } from 'hooks/useRecords';
 import { useTags } from 'hooks/useTags';
 import Icon from 'components/Icon';
 
 const Wrapper = styled.section`
     >header{
+        position:relative;
         display:flex;
         height:50px;
         align-items:center;
         justify-content:center;
         background:var(--skin-color);
         font-size:20px;
+        .back-icon{
+            position:absolute;
+            left:10px;
+            top:0;
+            bottom:0;
+            margin:auto;
+        }
         .icon{
             width:24px;
             height:24px;
@@ -23,11 +31,13 @@ const RecordDetail = () => {
     let id = parseInt(useParams<{ id: string }>().id);
     const { records } = useRecords();
     const { findTag } = useTags();
+    const history = useHistory();
     const record = records.find(v => v.id === id);
     const tag = findTag(record?.tagIds[0] || -1)
     return (<Wrapper>
         <header>
-            <Icon className="icon" name={tag?.icon} />
+            <Icon className="back-icon" name="left" onClick={() => { history.goBack() }} />
+            <Icon name={tag?.icon} />
             {tag?.name}
         </header>
     </Wrapper>);
