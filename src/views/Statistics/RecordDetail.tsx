d34c25ed527133dfb66bec45352849a9b923126b
jimmeyCheung <100178981@qq.com>
2020-08-14 23:17:21 +0800

    账单详情页样式调整，账单内容渲染

--- src/views/Statistics/RecordDetail.tsx
+++ src/views/Statistics/RecordDetail.tsx
@@ -4,8 +4,12 @@ import { useParams, useHistory } from 'react-router-dom';
 import { useRecords } from 'hooks/useRecords';
 import { useTags } from 'hooks/useTags';
 import Icon from 'components/Icon';
+import moment from 'moment';
 
 const Wrapper = styled.section`
+    display:flex;
+    flex-direction:column;
+    height:100vh;
     >header{
         position:relative;
         display:flex;
@@ -26,7 +30,34 @@ const Wrapper = styled.section`
             height:24px;
         }
     }
+    >main{
+        flex-shrink:1;
+        height:100%;
+        padding:20px 20px;
+        background:#fff;
+    }
 `;
+const ItemWrapper = styled.div`
+    display:flex;
+    border-bottom:1px solid var(--border-color);
+    padding:10px;
+    >.title{
+        padding-right:30px;
+        color:#8c8a8a;
+    }
+`;
+const getDay = (dayNumber: number) => {
+    const days = [
+        "星期一",
+        "星期二",
+        "星期三",
+        "星期四",
+        "星期五",
+        "星期六",
+        "星期天"
+    ];
+    return days[dayNumber - 1];
+}
 const RecordDetail = () => {
     let id = parseInt(useParams<{ id: string }>().id);
     const { records } = useRecords();
@@ -34,12 +65,31 @@ const RecordDetail = () => {
     const history = useHistory();
     const record = records.find(v => v.id === id);
     const tag = findTag(record?.tagIds[0] || -1)
+    const date = moment(record?.createdDate);
+    const items = [
+        { text: "类型", value: record?.category === "-" ? "支出" : "收入" },
+        { text: "金额", value: record?.amount + "元" },
+        { text: "时间", value: date.format("YYYY年M月D日") + " " + getDay(date.day()) },
+        { text: "备注", value: record?.note }
+    ];
     return (<Wrapper>
         <header>
             <Icon className="back-icon" name="left" onClick={() => { history.goBack() }} />
             <Icon name={tag?.icon} />
             {tag?.name}
         </header>
+        <main>
+            {
+                items.map(item => {
+                    return (
+                        <ItemWrapper>
+                            <span className="title">{item.text}</span>
+                            <span className="content">{item.value}</span>
+                        </ItemWrapper>
+                    );
+                })
+            }
+        </main>
     </Wrapper>);
 
 }
